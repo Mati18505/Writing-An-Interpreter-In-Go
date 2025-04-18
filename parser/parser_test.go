46366ed29986fb8db883461d9a0e5962554e6850
Mateusz Stulczewski <mstulczewski@gmail.com>
2025-04-18 17:46:46 +0200

    feat: Add parsing logic.
    test: Check parser errors.

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -11,14 +11,15 @@ import (
 
 func TestLetSatements(t *testing.T) {
 	input := `
-	let x = 5;
-	let y = 10;
-	let foobar = 838383;
+	let x 5;
+	let = 10;
+	let 838383;
 	`
 	l := lexer.New(input)
 	p := New(l)
 
 	program := p.ParseProgram()
+	checkParserErrors(t, p)
 
 	require.NotNil(t, program)
 	require.Equal(t, len(program.Statements), 3)
@@ -63,4 +64,19 @@ func testLetSatement(t *testing.T, s ast.Statement, name string) bool {
 	}
 
 	return true
+}
+
+func checkParserErrors(t *testing.T, p *Parser) {
+	errors := p.Errors()
+
+	if len(errors) == 0 {
+		return
+	}
+
+	t.Errorf("parser has %d errors", len(errors))
+
+	for _, msg := range errors {
+		t.Errorf("parser error: %q", msg)
+	}
+	t.FailNow()
 }
